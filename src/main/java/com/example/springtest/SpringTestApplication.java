58b4c48bf29dbb0b900f8d27bf0c578cd75f097e
hoang tran <hoangtq1607@gmail.com>
2022-07-17 12:10:10 +0700

    add comment

--- src/main/java/com/example/springtest/SpringTestApplication.java
+++ src/main/java/com/example/springtest/SpringTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy(proxyTargetClass=true)
 @SpringBootApplication
 public class SpringTestApplication {
-
+    // comment
     public static void main(String[] args) {
         SpringApplication.run(SpringTestApplication.class, args);
     }
